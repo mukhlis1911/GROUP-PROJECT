620f9067dd051b098bc75159950cc2efd7a2820f
repeattaktudeh <45623783+repeattaktudeh@users.noreply.github.com>
2021-06-12 02:23:26 +0800

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -35,7 +35,7 @@ export default class HomeScreen extends Component {
 
   render() {
     return (
-      <Container style={{backgroundColor:"floralwhite"}}>
+      <Container style={{backgroundColor:"mintcream"}}>
           <View style={{alignSelf: "flex-end", width: 50}}>
               <Button style={{backgroundColor:"firebrick"}} onPress={() => {this.goToLoginScreen()}}>
               <Icon name="log-out" style={{color: "white"}}/>
