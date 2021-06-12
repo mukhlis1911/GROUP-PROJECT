2d6645ec59df4c60510806d6beb55f67b503777b
xsab <81972587+xsab@users.noreply.github.com>
2021-06-13 01:09:00 +0800

    Update EventScreen.js

--- src/screens/EventScreen.js
+++ src/screens/EventScreen.js
@@ -45,7 +45,48 @@ export default class HomeScreen extends Component {
           </View>
 
         <Content padder>
-        {/* letak content korang dalam ni */}
+        
+        <View style={{height:120, width: 300, marginBottom:30, alignSelf: 'center', marginTop: 20, backgroundColor:"lightgrey", borderRadius:10}}>
+          
+            <View>
+                <Text style={{fontStyle:'bold', fontSize:14,  marginLeft:15, marginRight:15}}>Program’s name: [Diari Ramadan 1] Orang Asli: Realiti yang Tersingkap</Text>
+                <Text style={{fontStyle:'bold', fontSize:14,  marginLeft:15, marginRight:15, height: 20}}>Date: 17.4.2021</Text>
+                <Text style={{fontStyle:'bold', fontSize:14,  marginLeft:15, marginRight:15}}>Time: 2.30 pm - 3.30 pm</Text>
+                <Text style={{fontStyle:'bold', fontSize:14,  marginLeft:15, marginRight:15}}>Description: Ustaz Zaini Dahlan</Text>
+              
+        </View>
+        </View>
+
+        <View style={{height:200, width: 300, marginBottom:30, alignSelf: 'center', marginTop: 20, backgroundColor:"lightgrey", borderRadius:10}}>
+          
+          <View>
+              <Text style={{fontStyle:'bold', fontSize:14,  marginLeft:15, marginRight:15}}>Program’s name: [Diari Ramadan 2]  How To become budiman)</Text>
+              <Text style={{fontStyle:'bold', fontSize:14,  marginLeft:15, marginRight:15, height: 20}}>Date: 24/4/2021</Text>
+              <Text style={{fontStyle:'bold', fontSize:14,  marginLeft:15, marginRight:15}}>Time: 10.00 am- 12.00 pm</Text>
+              <Text style={{fontStyle:'bold', fontSize:14,  marginLeft:15, marginRight:15}}>Description: The best people are those who are most beneficial to others</Text>
+              <Text style={{fontStyle:'bold', fontSize:14,  marginLeft:15, marginRight:15}}>SPEAKER : BR. FIRDAUS WONG , SR.HELIZA HELMI AND SR.HAZWANI HELMI</Text>
+      </View>
+      </View>
+
+      <View style={{height:120, width: 300, marginBottom:30, alignSelf: 'center', marginTop: 20, backgroundColor:"lightgrey", borderRadius:10}}>
+          
+          <View>
+              <Text style={{fontStyle:'bold', fontSize:14,  marginLeft:15, marginRight:15}}>Program’s name: [Diari Ramadan 3] Lailatul Qadar</Text>
+              <Text style={{fontStyle:'bold', fontSize:14,  marginLeft:15, marginRight:15, height: 20}}>Date: 1.5.2021</Text>
+              <Text style={{fontStyle:'bold', fontSize:14,  marginLeft:15, marginRight:15}}>Time: 2.30 pm</Text>
+              <Text style={{fontStyle:'bold', fontSize:14,  marginLeft:15, marginRight:15}}>Description: Ustaz Hamid Jusoh</Text>
+      </View>
+      </View>
+
+      <View style={{height:250, width: 300, marginBottom:30, alignSelf: 'center', marginTop: 20, backgroundColor:"lightgrey", borderRadius:10}}>
+          
+          <View>
+              <Text style={{fontStyle:'bold', fontSize:14,  marginLeft:15, marginRight:15}}>Programs’s name: Jasa’s Sharing Islamic Knowledge Among People (SIKAP 1.0) Online</Text>
+              <Text style={{fontStyle:'bold', fontSize:14,  marginLeft:15, marginRight:15, height: 20}}>Date: 5.5.2021</Text>
+              <Text style={{fontStyle:'bold', fontSize:14,  marginLeft:15, marginRight:15}}>Time: 2.30 pm-3.30 pm </Text>
+              <Text style={{fontStyle:'bold', fontSize:14,  marginLeft:15, marginRight:15}}>Description: SIKAP ialah sebuah program yang melibatkan sesi perkongsian mengenai pengalaman dan kisah bersama orang asli. Pada kali ini, Br Mubarak iaitu Alumni Kelab JASA, akan  berkongsi ilmu mengenai keunikan orang asli bersama kita.</Text>
+      </View>
+      </View>
         </Content>
 
         <Footer>
