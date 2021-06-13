c66e8c957f5e3eb7e5721ac2eaaa757939000feb
repeattaktudeh <45623783+repeattaktudeh@users.noreply.github.com>
2021-06-13 20:29:10 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import ListScreen from './src/screens/ListScreen';
-import NewScreen from './src/screens/NewScreen';
-import ViewScreen from './src/screens/ViewScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 import HomeScreen from './src/screens/HomeScreen';
@@ -11,14 +8,10 @@ import EventScreen from './src/screens/EventScreen';
 import AboutScreen from './src/screens/AboutScreen';
 import AchievementScreen from './src/screens/AchievementScreen';
 
-
 const RootStack = createStackNavigator(
   {
     Signup: SignupScreen,
     Login: LoginScreen,
-    View: ViewScreen,
-    New: NewScreen,
-    List: ListScreen,
     Home: HomeScreen,
     Event: EventScreen,
     About: AboutScreen,
